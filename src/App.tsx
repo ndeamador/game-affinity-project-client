d297b015d9b81aeec3b895bbabdab92e7f99677e
ndeamador <ndeamador@gmail.com>
2021-03-25 21:56:38 +1100

    feat: Added login/register modals
    - Reach UI component for modal
    - Added react-hook-forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import SearchBar from './components/SearchBar';
 
 import { useLazyQuery } from '@apollo/client';
 import { FIND_GAMES } from './queries';
 
+import '@reach/dialog/styles.css';
+import { Dialog } from '@reach/dialog';
+
 import GameList from './components/GameList';
+import SearchBar from './components/SearchBar';
+import LoginForm from './components/LoginForm';
 
 function App() {
   const [findGames, result] = useLazyQuery(FIND_GAMES);
   const [games, setGames] = useState([]);
   const [query, setQuery] = useState('');
 
+  // MODALS
+  const [openModal, setOpenModal] = useState('none');
+
+  // GAME SEARCH
   const searchGames = (name: string) => {
     findGames({ variables: { name } });
   };
@@ -23,9 +31,9 @@ function App() {
 
   console.log('useState games:', games);
 
-  // Temp search logic
-  const handleGameQueryChange = (
-    event: React.ChangeEvent<HTMLInputElement>
+  // TEMP SEARCH LOGIC
+  const handleGameQueryChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event
   ) => {
     setQuery(event.target.value);
   };
@@ -36,8 +44,44 @@ function App() {
     }
   }, [query]);
 
+  // REGISTER AND LOGIN
+  const login = () => {
+    console.log('login');
+  };
+
+  const register = () => {
+    console.log('register');
+  };
+
   return (
     <div className='App'>
+      <div style={{ display: 'flex' }}>
+        <h3>TEMP NAV</h3>
+        <div>
+          <button onClick={() => setOpenModal('login')}>Login</button>
+        </div>
+        <div>
+          <button onClick={() => setOpenModal('register')}>Register</button>
+        </div>
+        <Dialog aria-label='Login form' isOpen={openModal === 'login'}>
+          <div>
+            <button onClick={() => setOpenModal('none')}>Close</button>
+          </div>
+          <h3>Login</h3>
+          <LoginForm onSubmit={login} buttonLabel='Login' />
+        </Dialog>
+        <Dialog
+          aria-label='Registration form'
+          isOpen={openModal === 'register'}
+        >
+          <div>
+            <button onClick={() => setOpenModal('none')}>Close</button>
+          </div>
+          <h3>Register</h3>
+          <LoginForm onSubmit={register} buttonLabel='Register' />
+        </Dialog>
+      </div>
+
       <SearchBar
         handleQueryChange={handleGameQueryChange}
         placeholderText='Find a game...'
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+interface LoginFormProps {
+  onSubmit: any;
+  buttonLabel: string;
+}
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const LoginForm = ({ onSubmit, buttonLabel }: LoginFormProps) => {
+  const { register, handleSubmit, errors } = useForm<FormData>(); // React-hook-form
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          placeholder='Sign up with your email'
+          ref={register({ required: true })}
+          aria-invalid={errors.email ? 'true' : 'false'}
+        />
+        {errors.email && <p>Handle this email error</p>}
+      </div>
+      <div>
+        <input
+          id='password'
+          name='password'
+          type='password'
+          placeholder='Password'
+          ref={register({ required: true, minLength: 8, maxLength: 64 })}
+          aria-invalid={errors.password ? 'true' : 'false'}
+        />
+        {errors.password && <p>Handle this password error</p>}
+      </div>
+      <div>
+        <button type='submit'>{buttonLabel}</button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
