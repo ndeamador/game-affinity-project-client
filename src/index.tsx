e02e59d7a9c6b576d15937d22f41960f424f787a
ndeamador <50785597+ndeamador@users.noreply.github.com>
2021-05-25 12:27:32 +1000

    Dev (#5)
    * Vercel test
    
    * Vercel test

--- src/index.tsx
+++ src/index.tsx
@@ -11,13 +11,12 @@ import {
 
 import { AuthProvider } from './context/AuthContext';
 
-console.log('ENVIROMENT: ', process.env.API_URL);
+console.log('ENVIROMENT: ', process.env.REACT_APP_VERCEL_API_URL);
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
     uri: process.env.REACT_APP_VERCEL_API_URL,
-    // uri: 'gap-server.nicodeamador.com/graphql/',
     credentials: 'include', // Required for sessions. Sends our cookie back to the server with every request. // https://www.apollographql.com/docs/react/networking/authentication/
   }),
 });
