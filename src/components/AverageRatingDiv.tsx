6cfed8be7f659bf081176514b7e97bb175b313bd
ndeamador <ndeamador@gmail.com>
2021-12-01 07:32:06 +1100

    refactor: extracted AvgRatingDiv styles out of component

--- src/components/AverageRatingDiv.tsx
+++ src/components/AverageRatingDiv.tsx
@@ -1,8 +1,43 @@
 /** @jsxImportSource @emotion/react */
 
+import { css, SerializedStyles } from '@emotion/react';
+
+const style = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  font: '20px sans-serif',
+  fontWeight: 'bold',
+  color: 'white',
+  // backgroundColor: backgroundColor(rating100Scale),
+  width: '90px',
+  height: '90px',
+  maxwidth: '90px',
+  borderRadius: 'var(--border-radius)',
+  marginRight: '10px',
+});
+
+const backgroundColor: { [key: string]: SerializedStyles } = {
+  green: css({
+    backgroundColor: 'var(--color-green)',
+  }),
+  yellowgreen: css({
+    backgroundColor: 'var(--color-yellowgreen)',
+  }),
+  gold: css({
+    backgroundColor: 'var(--color-gold)',
+  }),
+  orange: css({
+    backgroundColor: 'var(--color-orange)',
+  }),
+  red: css({
+    backgroundColor: 'var(--color-danger)',
+  }),
+};
+
 const AverageRatingDiv = ({ rating }: { rating: number }) => {
   const rating100Scale = Math.round((rating * 100) / 3);
-  const backgroundColor = (rating: number): string => {
+  const getBackgroundColor = (rating: number): string => {
     if (rating >= 85) return 'green';
     else if (rating >= 70) return 'yellowgreen';
     else if (rating >= 50) return 'gold';
@@ -13,21 +48,7 @@ const AverageRatingDiv = ({ rating }: { rating: number }) => {
   return (
     <div
       className='AvgRatingDiv'
-      css={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        font: '20px sans-serif',
-        fontWeight: 'bold',
-        color: 'white',
-        backgroundColor: backgroundColor(rating100Scale),
-        width: '90px',
-        height: '90px',
-        // height: '100%',
-        maxwidth: '90px',
-        borderRadius: 'var(--border-radius)',
-        marginRight: '10px',
-      }}
+      css={[style, backgroundColor[getBackgroundColor(rating100Scale)]]}
     >
       {rating100Scale}
     </div>
