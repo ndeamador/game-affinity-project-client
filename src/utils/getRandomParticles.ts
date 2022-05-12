5302c3bcc8c9ab770a6b2cdcc504efe578ca4b43
ndeamador <ndeamador@gmail.com>
2022-05-12 15:54:14 +1000

    tweak: added sizeVariation variable to particle size

--- src/utils/getRandomParticles.ts
+++ src/utils/getRandomParticles.ts
@@ -5,14 +5,15 @@ const getRandomParticles = (
 ): AnimatedParticleBaseProps[] => {
   const particlesArray: AnimatedParticleBaseProps[] = [];
   const particleMultiplier = 10;
-  // const numberOfParticles = particleMultiplier * (windowSize.height * windowSize.width) / 100000
-  const numberOfParticles = 50;
-  const speedMultiplier = 3; //3do
-  const sizeMultiplier = 1;
+  const numberOfParticles = particleMultiplier * (windowSize.height * windowSize.width) / 100000
+  // const numberOfParticles = 50;
+  const speedMultiplier = 5; //3 0.3
+  const sizeMultiplier = 1; // used as radius for canvas.arc(). Unit not pixels.
+  const sizeVariation = 3 // 5
 
 
   for (let i = 0; i < numberOfParticles; i++) {
-    const size = sizeMultiplier * Math.random() * 5 + 1; // random number between 1 and 5
+    const size = sizeMultiplier * (Math.random() * sizeVariation + 1); // random number between 1 and sizeVariation.
     // particle position set random number between 0 and canvas width/height with particle size defining a buffer around the canvas to avoid particles getting stuck in the edge.
     const x = Math.random() * (innerWidth - size * 2 - size * 2) + size * 2;
     const y = Math.random() * (innerHeight - size * 2 - size * 2) - size * 2;
