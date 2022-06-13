b16b59c7b51f7d8d31d3acac54af04e53ead2ce0
ndeamador <ndeamador@gmail.com>
2022-06-14 06:03:53 +1000

    feat: added spinning placeholder for loading cover images

--- src/components/CoverDiv.tsx
+++ src/components/CoverDiv.tsx
@@ -2,41 +2,76 @@
 import { Game } from '../types';
 import { CgGames } from 'react-icons/cg';
 import { css } from '@emotion/react';
+import { useState } from 'react';
+import { Spinner } from './styledComponentsLibrary';
 
-const containerStyle = css({
-  display: 'flex',
-  maxwidth: 'var(--cover-width)',
-  flexShrink: 0,
-});
-
-const imageStyle = css({
-  height: '100%',
-  borderRadius: 'var(--border-radius)',
-});
-
-const genericIconStyle = css({
-  // width: 'auto',
-  // maxwidth: 'var(--cover-width)',
-  // minWidth: 'var(--cover-width)',
-  width: 'var(--cover-width)',
-  height: 'var(--cover-width)',
-  // height: '100%',
-  border: 'solid 2px var(--color-gray20)',
-  borderRadius: 'var(--border-radius)',
-  padding: '8px',
-});
+const styles = {
+  mainContainer: css({
+    display: 'flex',
+    width: 'var(--cover-width)',
+    height: 'var(--cover-width)',
+    flexShrink: 0,
+  }),
+  image: css({
+    height: '100%',
+    borderRadius: 'var(--border-radius)',
+  }),
+  genericBox: css({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 'var(--cover-width)',
+    height: 'var(--cover-width)',
+    border: 'solid 2px var(--inner-border-color)',
+    borderRadius: 'var(--border-radius)',
+    padding: '8px',
+  }),
+  genericIcon: css({
+    width: '100%',
+    height: 'auto',
+    // width: 'var(--cover-width)',
+    // height: 'var(--cover-width)',
+  }),
+  spinner: css({ width: '50%', height: '50%' }),
+};
 
 const CoverDiv = ({ game }: { game: Game }) => {
+  const [loaded, setLoaded] = useState(false);
+
   // Setting image resolution from url: https://api-docs.igdb.com/#images
   const imageSize = 'thumb';
   const imageLink = `//images.igdb.com/igdb/image/upload/t_${imageSize}/${game.cover?.image_id}.jpg`;
 
   return (
-    <div css={containerStyle}>
+    // <div css={styles.mainContainer}>
+    //   {game.cover ? (
+    //     <img src={imageLink} css={styles.image} onLoad={(event: any) => {console.log('load: ', event); setLoaded(true)}} />
+    //   ) : (
+    //     <CgGames css={styles.genericIcon} />
+    //   )}
+    // </div>
+
+    <div css={[styles.mainContainer]}>
       {game.cover ? (
-        <img src={imageLink} css={imageStyle} />
+        <>
+          <img
+            src={imageLink}
+            css={styles.image}
+            onLoad={(event: any) => {
+              console.log('load: ', event);
+              setLoaded(true);
+            }}
+          />
+          {!loaded && (
+            <div css={styles.genericBox}>
+              <Spinner css={styles.spinner} />
+            </div>
+          )}
+        </>
       ) : (
-        <CgGames css={genericIconStyle} />
+        <div css={styles.genericBox}>
+          <CgGames css={styles.genericIcon} />
+        </div>
       )}
     </div>
   );
