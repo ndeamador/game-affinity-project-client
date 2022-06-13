153122ca3471f6822aa5166f3759178a8e29376d
ndeamador <ndeamador@gmail.com>
2022-06-14 03:35:00 +1000

    tweak: search results collapse now handled by Home graphql query

--- src/hooks/useClickedOutOfElement.ts
+++ src/hooks/useClickedOutOfElement.ts
@@ -4,18 +4,21 @@ const useClickedOutOfElement = (ref: React.RefObject<HTMLElement>) => {
   const [clickedOutOfElement, setClickedOutOfElement] = useState(false);
 
   const handleClickOutside = (e: any) => {
+    // e.target returns the html element that has been clicked.
     if (ref.current && !ref.current.contains(e.target)) {
       setClickedOutOfElement(true);
     }
+    else {
+      setClickedOutOfElement(false);
+    }
   };
-  const handleClickInside = () => setClickedOutOfElement(false);
 
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => document.removeEventListener('mousedown', handleClickOutside);
   });
 
-  return [clickedOutOfElement, () => handleClickInside()];
+  return [clickedOutOfElement] as const;
 }
 
 export default useClickedOutOfElement;
