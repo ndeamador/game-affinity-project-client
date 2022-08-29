48b7d6f282bdae3b475d015ff5580eb4dc97d8b0
ndeamador <ndeamador@gmail.com>
2022-08-29 17:37:17 +1000

    feat: added animated border to test loging button

--- src/components/LoginWithTestAccountButton.tsx
+++ src/components/LoginWithTestAccountButton.tsx
@@ -1,9 +1,40 @@
 /** @jsxImportSource @emotion/react */
 
 import { useMutation } from '@apollo/client';
+import { css, keyframes } from '@emotion/react';
 import { LOGIN } from '../graphql/mutations';
 import { CURRENT_USER } from '../graphql/queries';
 import { Button } from './styledComponentsLibrary';
+
+const animations = {
+  shine: keyframes`
+  to {
+    background-position: 200% center;
+  }
+  `,
+};
+
+const styles = {
+  animatedBorder: css({
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 'var(--border-radius)',
+    background: `linear-gradient(40deg,
+        var(--button-gradient-border-color-1) 20%,
+        var(--button-gradient-border-color-1) 40%,
+        var(--button-gradient-border-color-2) 50%,
+        var(--button-gradient-border-color-2) 55%,
+        var(--button-gradient-border-color-1) 70%,
+        var(--button-gradient-border-color-1) 100%)`,
+    backgroundSize: '200% auto', // https://codersblock.com/blog/gradient-animation-trick/
+    animation: `${animations.shine} 2s linear infinite`,
+  }),
+  button: css({
+    backgroundColor: 'var(--button-gradient-border-color-1)',
+    margin: '2px',
+    // opacity: 0,
+  }),
+};
 
 const LonginWithTestAccountButton = () => {
   const [login, { loading: loginLoading, error: loginError }] = useMutation(
@@ -28,7 +59,13 @@ const LonginWithTestAccountButton = () => {
     });
   };
 
-  return <Button onClick={() => submitLogin()}>Login with test account</Button>;
+  return (
+    <div css={styles.animatedBorder}>
+      <Button css={styles.button} onClick={() => submitLogin()}>
+        Login with test account
+      </Button>
+    </div>
+  );
 };
 
 export default LonginWithTestAccountButton;
