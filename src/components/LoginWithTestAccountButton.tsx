732736cc693947e611383142e312c387600c2be1
ndeamador <ndeamador@gmail.com>
2022-10-05 15:00:11 +1100

    minor: styling

--- src/components/LoginWithTestAccountButton.tsx
+++ src/components/LoginWithTestAccountButton.tsx
@@ -22,6 +22,11 @@ const styles = {
     background: 'var(--animated-border-gradient)',
     backgroundSize: '200% auto', // https://codersblock.com/blog/gradient-animation-trick/
     animation: `${animations.borderGlow} 2s linear infinite`,
+    '&:hover': {
+      background: 'var(--animated-border-gradient-hover)',
+      animation: `${animations.borderGlow} 2s linear infinite`,
+      backgroundSize: '200% auto',
+    },
   }),
   button: css({
     backgroundColor: 'var(--button-gradient-border-color-1)',
