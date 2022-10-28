6c01274e743ec646187c6453f0f00826b08dc65c
ndeamador <ndeamador@gmail.com>
2022-10-28 11:44:49 +1100

    minor: replaced any types with specific types

--- src/components/views/Library/DraggablePortalHandler.tsx
+++ src/components/views/Library/DraggablePortalHandler.tsx
@@ -1,12 +1,19 @@
-import ReactDOM from "react-dom";
+import { DraggableStateSnapshot } from 'react-beautiful-dnd';
+import ReactDOM from 'react-dom';
 
 // backdrop-filter causes draggable items to offset out of place, this is a workaround for the bug.
 // https://github.com/atlassian/react-beautiful-dnd/issues/499#issuecomment-867282432
 
-const DraggablePortalHandler = ({ children, snapshot }: { children: any, snapshot: any }) => {
-    if (snapshot.isDragging)
-        return ReactDOM.createPortal(children, document.body);
-    return children;
-}
+const DraggablePortalHandler = ({
+  children,
+  snapshot,
+}: {
+  children: JSX.Element;
+  snapshot: DraggableStateSnapshot;
+}) => {
+  if (snapshot.isDragging)
+    return ReactDOM.createPortal(children, document.body);
+  return children;
+};
 
-export default DraggablePortalHandler
+export default DraggablePortalHandler;
